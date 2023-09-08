0669661b44c6cadcfbe7d5a50c2d2c25d35b952e
Bryk <bryk.defi@gmail.com>
2023-09-07 21:54:00 -0700

    main

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a sentence with proper grammar.
+ *              The outcome is a piece of art.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
