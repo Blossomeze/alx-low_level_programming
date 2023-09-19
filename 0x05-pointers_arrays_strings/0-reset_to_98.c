e48a756a0554517766a5f64edf55ce3d96f95bfe
Bryk <bryk.defi@gmail.com>
2023-09-18 21:12:56 -0700

    main

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include main.h
+
+/**
+ * main - take in a pointer to an int as parameter and updates the value it points to to 98
+ *
+ * Return: Always n
+ */
+
+void reset_to_98(int *n){
+	*n=98;
+}
