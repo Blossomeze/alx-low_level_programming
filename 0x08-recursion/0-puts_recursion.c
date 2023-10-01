5c27d64fd47ae8f5e1d142f1465fcf0e270b4672
Bryk <bryk.defi@gmail.com>
2023-10-01 16:13:05 -0700

    prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 
 /**
- * _puts_recursion - Prints a  string
+ * _puts_recursion - Prints a string recursively
  *
  * @s: pointer to the string to be printed
  *
- * Return:  Always 0
+ * Return: Always 0
  */
-void _puts_recursion(char *s) {
+void _puts_recursion(char *s)
+{
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion (s + 1);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
