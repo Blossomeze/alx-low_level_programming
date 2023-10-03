f0598f62e7fc5352100cb2ad7917725880760dc8
Bryk <bryk.defi@gmail.com>
2023-10-02 19:31:31 -0700

    C - argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h" // Include necessary header file (if required)
+#include "main.h"
 
 /**
- * main - prints the minimum number of coins to make change for an amount of money
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
  * @argc: number of arguments
  * @argv: array of arguments
  *
